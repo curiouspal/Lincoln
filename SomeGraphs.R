f871457d68623873a4ba70e87249c0ab90e2ac80
Anirban Pal <bokaroseani@gmail.com>
2015-07-17 11:58:46 -0700

    .

--- SomeGraphs.R
+++ SomeGraphs.R
@@ -1,4 +1,7 @@
+
+library("ggplot2")
 allcities <- read.csv("landdata-msas-2015q1.csv")
+
 for(i in 1:length(allcities$Home.Value)) {
   b <- as.vector(strsplit(as.character(allcities$Home.Value[i]), "$", fixed = TRUE))
   allcities$Home.Value1[i] <- b[[1]][2]
@@ -19,8 +22,10 @@ for(i in 1:length(allcities$Land.Value1)) {
 }
 allcities$Land.Value1 <- as.numeric(allcities$Land.Value1)
 
-library("ggplot2")
 portland <- subset(allcities, MSA=="PORTLAND" | MSA=="DENVER" | MSA=="BOSTON" & as.character(Date)>"1985Q1")
+
+
+
 
 ggplot(data=allcities, aes(Date, Land.Price.Index)) + geom_point() + 
   theme(
@@ -91,3 +96,13 @@ ggplot(data=portland, aes(Date, Land.Value1)) + geom_point(aes(color=MSA)) +
     legend.text = element_text(colour="#000033", size = 18)
   ) 
 
+ggplot(data=portland, aes(Date, Land.Share..Pct.)) + geom_point(aes(color=MSA)) + 
+  theme(
+    plot.title = element_text(size=20, face="bold", vjust=3, color="#000033"), 
+    axis.text.x = element_text(size=12, angle = 90, hjust = 1, color="#000033"), 
+    axis.text.y = element_text(size=8), 
+    axis.title.x = element_text(color="#000033", size=18), 
+    axis.title.y = element_text(color="#000033", size=18, vjust=2),
+    legend.title=element_blank(),
+    legend.text = element_text(colour="#000033", size = 18)
+  ) 
