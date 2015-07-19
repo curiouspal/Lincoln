0c730eb626807413e79331e8f41f9dbaa6a1ea75
Anirban Pal <bokaroseani@gmail.com>
2015-07-19 00:21:00 -0700

    fixed some of the character type data to make them numeric.

--- SomeGraphs.R
+++ SomeGraphs.R
@@ -22,6 +22,23 @@ for(i in 1:length(allcities$Land.Value1)) {
 }
 allcities$Land.Value1 <- as.numeric(allcities$Land.Value1)
 
+
+for(i in 1:length(allcities$Date)) {
+  b <- as.vector(strsplit(as.character(allcities$Date[i]), "Q", fixed = TRUE))
+  allcities$Date1[i] <- as.numeric(b[[1]][1]) + (as.numeric(b[[1]][2])*0.25) 
+}
+
+
+for(i in 1:length(allcities$Land.Share..Pct.)) {
+  b <- as.vector(strsplit(as.character(allcities$Land.Share..Pct.[i]), "%", fixed = TRUE))
+  allcities$Land.Share1[i] <- b[[1]][1]
+}
+for(i in 1:length(allcities$Land.Share1)) {
+  b <- as.vector(strsplit(as.character(allcities$Land.Share1[i]), ".", fixed = TRUE))
+  allcities$Land.Share1[i] <- paste(b[[1]][1], b[[1]][2], sep = "")
+}
+allcities$Land.Share1 <- as.numeric(allcities$Land.Share1)/10
+
 portland <- subset(allcities, MSA=="PORTLAND" | MSA=="DENVER" | MSA=="BOSTON" & as.character(Date)>"1985Q1")
 
 
@@ -85,7 +102,7 @@ ggplot(data=portland, aes(Date, Home.Value1)) + geom_point(aes(color=MSA)) +
     legend.text = element_text(colour="#000033", size = 18)
   ) 
 
-ggplot(data=portland, aes(Date, Land.Value1)) + geom_point(aes(color=MSA)) + 
+ggplot(data=portland, aes(Date1, Land.Value1)) + geom_line(aes(color=MSA)) + 
   theme(
     plot.title = element_text(size=20, face="bold", vjust=3, color="#000033"), 
     axis.text.x = element_text(size=12, angle = 90, hjust = 1, color="#000033"), 
@@ -96,7 +113,7 @@ ggplot(data=portland, aes(Date, Land.Value1)) + geom_point(aes(color=MSA)) +
     legend.text = element_text(colour="#000033", size = 18)
   ) 
 
-ggplot(data=portland, aes(as.numeric(Date), as.numeric(Land.Share..Pct.))) + geom_line(stat="identity", aes(color=MSA)) + 
+ggplot(data=portland, aes((Date1), (Land.Share1))) + geom_line(stat="identity", aes(color=MSA)) + 
   theme(
     plot.title = element_text(size=20, face="bold", vjust=3, color="#000033"), 
     axis.text.x = element_text(size=12, angle = 90, hjust = 1, color="#000033"), 
@@ -106,3 +123,4 @@ ggplot(data=portland, aes(as.numeric(Date), as.numeric(Land.Share..Pct.))) + geo
     legend.title=element_blank(),
     legend.text = element_text(colour="#000033", size = 18)
   ) 
+
